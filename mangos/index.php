78a38922ffb92bde3ee9c03d37d6e1e832e93cd4
klyxmaster <klyxmaster@2d78c92d-6506-4d2c-a6df-e9d63a7768dd>
2011-04-18 19:26:41 +0000

    - index typo for installer initiater

--- mangos/index.php
+++ mangos/index.php
@@ -1,7 +1,8 @@
 <?php
 	if ( ! file_exists('include/config.php'))
 	{
-		header('location:include/install.php'));
+		header('location:include/install.php');
+		die();
 	}
 	header('location:quest_search.php');
 ?>
